c4e64471702343b43db519abdff0484365141fd5
EviePryler <eviepryler@gmail.com>
2014-05-16 23:38:10 -0500

    New questlogs and beginning npc work for Cisterns

---
+++ quest/jeremiah_hornanfeld_187_cadomyr.lua
@@ -0,0 +1,203 @@
+--[[
+Illarion Server
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU Affero General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+details.
+
+You should have received a copy of the GNU Affero General Public License along
+with this program.  If not, see <http://www.gnu.org/licenses/>. 
+]]
+-- INSERT INTO "quests" ("qst_id", "qst_script") VALUES (187, 'quest.jeremiah_hornanfeld_187_cadomyr');
+
+require("base.common")
+require("base.factions")
+module("quest.jeremiah_hornanfeld_187_cadomyr", package.seeall)
+
+GERMAN = Player.german
+ENGLISH = Player.english
+
+-- Insert the quest title here, in both languages
+Title = {}
+Title[GERMAN] = ""
+Title[ENGLISH] = "Vermin Monitor"
+
+-- Insert an extensive description of each status here, in both languages
+-- Make sure that the player knows exactly where to go and what to do
+Description = {}
+Description[GERMAN] = {}
+Description[ENGLISH] = {}
+Description[GERMAN][1] = ""
+Description[ENGLISH][1] = "Kill 5 rats in the Cistern, you still have 5 left."
+Description[GERMAN][2] = ""
+Description[ENGLISH][2] = "Kill 5 rats in the Cistern, you still have 4 left."
+Description[GERMAN][3] = ""
+Description[ENGLISH][3] = "Kill 5 rats in the Cistern, you still have 3 left."
+Description[GERMAN][4] = ""
+Description[ENGLISH][4] = "Kill 5 rats in the Cistern, you still have 2 left."
+Description[GERMAN][5] = ""
+Description[ENGLISH][5] = "Kill 5 rats in the Cistern, you still have 1 left."
+Description[GERMAN][6] = "."
+Description[ENGLISH][6] = "Return to Jeremiah Hornanfeld for you reward."
+
+Description[GERMAN][7] = ""
+Description[ENGLISH][7] = "Check back with Rat Monitor Jeremiah Hornanfeld, he may have another task for you."
+
+Description[GERMAN][8] = ""
+Description[ENGLISH][8] = "Collect ten rat furs and bring them to Jeremiah Hornanfeld as evidence."
+Description[GERMAN][9] = ""
+Description[ENGLISH][9] = "Perhaps Jeremiah Hornanfeld has another task to be done."
+
+Description[GERMAN][10] = ""
+Description[ENGLISH][10] = "Kill 10 Agressive and Muskrats, you still need 10."
+Description[GERMAN][11] = ""
+Description[ENGLISH][11] = "Kill 10 Agressive and Muskrats, you still need 9."
+Description[GERMAN][12] = ""
+Description[ENGLISH][12] = "Kill 10 Agressive and Muskrats, you still need 8."
+Description[GERMAN][13] = ""
+Description[ENGLISH][13] = "Kill 10 Agressive and Muskrats, you still need 7."
+Description[GERMAN][14] = ""
+Description[ENGLISH][14] = "Kill 10 Agressive and Muskrats, you still need 6."
+Description[GERMAN][15] = ""
+Description[ENGLISH][15] = "Kill 10 Agressive and Muskrats, you still need 5."
+Description[GERMAN][16] = ""
+Description[ENGLISH][16] = "Kill 10 Agressive and Muskrats, you still need 4."
+Description[GERMAN][17] = ""
+Description[ENGLISH][17] = "Kill 10 Agressive and Muskrats, you still need 3."
+Description[GERMAN][18] = ""
+Description[ENGLISH][18] = "Kill 10 Agressive and Muskrats, you still need 2."
+Description[GERMAN][19] = ""
+Description[ENGLISH][19] = "Kill 10 Agressive and Muskrats, you still need 1."
+Description[GERMAN][20] = ""
+Description[ENGLISH][20] = "Return to Jeremiah Hornanfeld for you reward."
+
+Description[GERMAN][21] = ""
+Description[ENGLISH][21] = "Check back with Rat Monitor Jeremiah Hornanfeld, he may have another task for you."
+
+Description[GERMAN][22] = ""
+Description[ENGLISH][22] = "Collect 25 potash from the Cistern rats."
+Description[GERMAN][23] = ""
+Description[ENGLISH][23] = "Perhaps Jeremiah Hornanfeld has another task to be done."
+
+Description[GERMAN][24] = ""
+Description[ENGLISH][24] = "Kill 10 Sewer Rats, you still need 10."
+Description[GERMAN][25] = ""
+Description[ENGLISH][25] = "Kill 10 Sewer Rats, you still need 9."
+Description[GERMAN][26] = ""
+Description[ENGLISH][26] = "Kill 10 Sewer Rats, you still need 8."
+Description[GERMAN][27] = ""
+Description[ENGLISH][27] = "Kill 10 Sewer Rats, you still need 7."
+Description[GERMAN][28] = ""
+Description[ENGLISH][28] = "Kill 10 Sewer Rats, you still need 6."
+Description[GERMAN][29] = ""
+Description[ENGLISH][29] = "Kill 10 Sewer Rats, you still need 5."
+Description[GERMAN][30] = ""
+Description[ENGLISH][30] = "Kill 10 Sewer Rats, you still need 4."
+Description[GERMAN][31] = ""
+Description[ENGLISH][31] = "Kill 10 Sewer Rats, you still need 3."
+Description[GERMAN][32] = ""
+Description[ENGLISH][32] = "Kill 10 Sewer Rats, you still need 2."
+Description[GERMAN][33] = ""
+Description[ENGLISH][33] = "Kill 10 Sewer Rats, you still need 1."
+Description[GERMAN][34] = ""
+Description[ENGLISH][34] = "Return to Jeremiah Hornanfeld for you reward."
+
+Description[GERMAN][35] = ""
+Description[ENGLISH][35] = "Check back with Rat Monitor Jeremiah Hornanfeld, he may have another task for you."
+
+Description[GERMAN][36] = ""
+Description[ENGLISH][36] = "Collect 10 raw leather from the plague rats in the Cistern Cave and return them to Jeremiah Hornanfeld."
+Description[GERMAN][37] = ""
+Description[ENGLISH][37] = "You have fulfilled all of Jeremiah Hornanfeld's tasks."
+
+-- Insert the position of the quest start here (probably the position of an NPC or item)
+Start = {72, 647, 0}
+
+-- For each status insert a list of positions where the quest will continue, i.e. a new status can be reached there
+QuestTarget = {}
+QuestTarget[1] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[2] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[3] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[4] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[5] = {position(72, 647, 0), position(5025, 23, -3)} 
+QuestTarget[6] = {position(72, 647, 0), position(5025, 23, -3)}
+
+QuestTarget[7] = {position(72, 647, 0)}
+
+QuestTarget[8] = {position(72, 647, 0)}
+QuestTarget[9] = {position(72, 647, 0)}
+
+QuestTarget[10] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[11] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[12] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[13] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[14] = {position(72, 647, 0), position(5025, 23, -3)} 
+QuestTarget[15] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[16] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[17] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[18] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[19] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[20] = {position(72, 647, 0), position(5025, 23, -3)} 
+
+QuestTarget[21] = {position(72, 647, 0)}
+
+QuestTarget[22] = {position(72, 647, 0)}
+QuestTarget[23] = {position(72, 647, 0)}
+
+QuestTarget[24] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[25] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[26] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[27] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[28] = {position(72, 647, 0), position(5025, 23, -3)} 
+QuestTarget[29] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[30] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[31] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[32] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[33] = {position(72, 647, 0), position(5025, 23, -3)}
+QuestTarget[34] = {position(72, 647, 0), position(5025, 23, -3)} 
+
+QuestTarget[35] = {position(72, 647, 0)}
+
+QuestTarget[36] = {position(72, 647, 0)}
+QuestTarget[37] = {position(72, 647, 0)}
+
+-- Insert the quest status which is reached at the end of the quest
+FINAL_QUEST_STATUS = 37
+
+
+function QuestTitle(user)
+    return base.common.GetNLS(user, Title[GERMAN], Title[ENGLISH])
+end
+
+function QuestDescription(user, status)
+    local german = Description[GERMAN][status] or ""
+    local english = Description[ENGLISH][status] or ""
+
+    return base.common.GetNLS(user, german, english)
+end
+
+function QuestStart()
+    return Start
+end
+
+function QuestTargets(user, status)
+    return QuestTarget[status]
+end
+
+function QuestFinalStatus()
+    return FINAL_QUEST_STATUS
+end
+
+function QuestAvailability(user, status)
+    if base.factions.isCadomyrCitizen(user) and status == 0 then
+        return Player.questAvailable
+    else
+        return Player.questNotAvailable
+    end
+end
